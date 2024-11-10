b0ed10064ff566abc7c17c4b4764b8cb7dcea23f
AngelPineda1 <espinoaangel89@gmail.com>
2024-11-10 15:27:51 -0600

    ya se trae los tipos

---
+++ SRED/Pages/Admin/Tipos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SRED.Pages.Admin.Tipos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
