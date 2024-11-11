081dbbe2fe37c5e32596e5961ba32dd677d043ad
AngelPineda1 <espinoaangel89@gmail.com>
2024-11-11 15:24:43 -0600

    Crud de admin terminado

---
+++ SRED/Pages/Admin/Equipos/EditarEquipo(Lab).cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SRED.Pages.Admin.Equipos
+{
+    public class EditarEquipo_Lab_Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SRED/Pages/Admin/Equipos/EditarEquipo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SRED.Pages.Admin.Equipos
+{
+    public class EditarEquipoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
