682169b9e50c738c4d6c3ef314a9f6ecedfd2010
AngelPineda1 <espinoaangel89@gmail.com>
2024-12-18 01:47:54 -0600

    cambios

---
+++ SRED/Pages/Invitado/Confirmacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SRED.Pages.Invitado
+{
+    public class ConfirmacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
